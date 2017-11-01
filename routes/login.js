51f54ab7095971019cc1af57f986c89d49ddf17e
kekos91 <kekos91@free.fr>
2017-11-01 18:47:30 +0100

    login route on the server

---
+++ routes/login.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const randomstring = require("randomstring");
+
+router.post('/', function(req, res){
+    res.send({
+        'status': "OK",
+        'token' : randomstring.generate(64),
+        'body': req.body,
+        'test': 'ah'
+    });
+});
+
+module.exports = router;
